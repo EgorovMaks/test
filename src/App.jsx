0dee1bc061137b8eefdd329b5b64b80dae7e06cb
EgorovMaks <funt.egorov@gmail.com>
2023-08-04 17:51:08 +0500

    a

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import React, { useState } from "react";
+import axios from "axios";
 
 function App() {
+  const [selectedFile, setSelectedFile] = useState(null);
+
+  const handleFileChange = (event) => {
+    setSelectedFile(event.target.files);
+  };
+
+  const handleUpload = async () => {
+    for (let i = 0; i < selectedFile.length; i++) {
+      const { data } = await axios.get(
+        `https://cloud-api.yandex.net/v1/disk/resources/upload?path=%FuploadTest%2F${selectedFile[i].name}`,
+        {
+          headers: {
+            Authorization: `OAuth y0_AgAAAABqL_pFAApCVgAAAADo_2DGuUt5-PjvT2ivSXgyASIFjIv67cg`,
+          },
+        }
+      );
+      console.log(data)
+    }
+  };
+
   return (
-    <div className="App">
-      <h1>sadasdasd</h1>
+    <div>
+      <h2>Загрузка файла на Яндекс Диск</h2>
+      <input type="file" onChange={handleFileChange} multiple />
+      <button onClick={handleUpload}>Загрузить</button>
     </div>
   );
 }
